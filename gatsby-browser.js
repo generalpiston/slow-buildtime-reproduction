3b630417f3e5b8ab297a84cb7be1e3deb6000620
Abraham Elmahrek <abraham@elmahrek.com>
2020-12-15 13:59:37 -0800

    repro deps

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,9 @@
-import './src/styles/index.css';
+// import './src/styles/index.css';
+
+import 'tailwindcss/base.css';
+
+import 'tailwindcss/components.css';
+import './src/styles/box-button.css';
+
+import 'tailwindcss/utilities.css';
+import './src/styles/fontawesome.css';
